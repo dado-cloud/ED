14e93c73967d068344dcf1ed3709706a8ae5f745
dado-cloud <daad1mh@gmail.com>
2026-05-06 18:10:10 +0300

    Update test4.py

--- test4.py
+++ test4.py
@@ -3,7 +3,8 @@ from datetime import datetime, timezone, timedelta
 import base64
 import pandas as pd
 from pathlib import Path
-from utils.prediction import predict_daily, predict_hourly, monitoring
+from utils.prediction import predict_daily, predict_hourly
+from utils.monitoring import calculate_monitoring_metrics_from_df
 import re
 
 # ── Page config (only once) ──────────────────────────────────────────────────
